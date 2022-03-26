749fdfcadba61994dcafc71eaddb95bf150bd841
laptopadventure <bazelart13@gmail.com>
2022-03-26 13:10:13 -0700

    weather done, movies next

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const weatherData = require('./data/weather.json');
+const axios = require('axios');
 app.use(cors());
 
 const PORT = process.env.PORT || 3002;
@@ -16,15 +16,20 @@ class Forecast {
   }
 }
 
-app.get('/weather', (req, res, next) => {
-  const snipRegex = /([a-z]+)/i;
-  let sanitized_name = req.query.city.toLowerCase();
-  sanitized_name = sanitized_name.match(snipRegex)[0];
-  const findRegex = RegExp(sanitized_name);
-  const weatherCity = weatherData.find(loc => !!loc.city_name.toLowerCase().match(findRegex));
+app.get('/weather', async (req, res, next) => {
+  const reg = /[A-Z][a-z]+/;
+  const searchQuery = req.query.city;
+  const sanitized = searchQuery.match(reg)[0];
+  if(!sanitized) {
+    res.status(404).send('Could not find the weather lookup for your location.');
+    return;
+  }
   let forecast;
   try {
-    forecast = weatherCity.data.map(day => new Forecast(day.valid_date, day.weather.description));
+    const call = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&city=${sanitized}&lat=${parseInt(req.query.lat)}&lon=${parseInt(req.query.lon)}&days=3&units=I`;
+    const res = await axios.get(call);
+    console.log(res.data.data);
+    forecast = res.data.data.map(day => new Forecast(day.datetime, day.weather.description));
   } catch(error) {
     next(error);
   }
